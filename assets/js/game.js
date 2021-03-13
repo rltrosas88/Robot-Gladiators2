9bdf4852bdfea618ba4b86582e4a16a39807b43d
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-03-12 19:05:53 -0600

    1.6 Manage Data with Functions, Variables, and Operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,15 +2,45 @@
 
 var playerName = window.prompt("What is your robot's name?");
 // What is this?
-console.log(playerName);
+//console.log(playerName);
 //window.alert(playerName);
-console.log("This logs a string, good for leaving yourself a message");
+//console.log("This logs a string, good for leaving yourself a message");
 // this will do math and log 20
-console.log(10 +10);
+//console.log(10 +10);
 //what is this?
-console.log("Our robot's name is " + playerName);
+//console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    //Alert players that they are starting th eround
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaing."
+    );
+
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
 // this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
-//fight();
+//function fight() {
+    //window.alert("The fight has begun!");
+//}
+fight();
