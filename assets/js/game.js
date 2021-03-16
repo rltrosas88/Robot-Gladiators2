99fc35e935d40b98356c9bbe60c6a7c3d1f24a04
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-03-16 10:50:35 -0500

    2.5 Fight Enemy Robot Combatants Using for Loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,6 +27,19 @@ console.log(enemyNames);
 console.log(enemyNames[0]);
 console.log(enemyNames[1]);
 console.log(enemyNames[2]);
+console.log(enemyNames.length);
+//for(var i = 0; i < 3; i++) {
+    //console.log("apple", i);
+//}
+//for (var i = 0; i < enemyNames.length; i++) {
+    //console.log(enemyNames[i]);
+//}
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
